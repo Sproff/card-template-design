d331830acae953953c23fbd9be211b99d3dae209
Sproff <sproff.oluwaseun@gmail.com>
2021-02-06 00:42:02 +0100

    consumed the API

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Container, Box, Text, Image, Badge} from '@chakra-ui/react';
-// import {productArray} from './product';
+import axios from 'axios';
+
 import pr5 from '../assets/images/pr5.jpg';
 
 function CardProduct() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const APP_URL =
+      'https://cors-anywhere.herokuapp.com/https://ecommerce-product-api.herokuapp.com/';
+
+    axios
+      .get(APP_URL)
+      .then((res) => {
+        console.log(res.data);
+        setProducts(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Container bg="#fafafa" height="100%">
       <Text
@@ -19,48 +35,55 @@ function CardProduct() {
       >
         Card Template Design
       </Text>
-      <Box
-        width="300px"
-        height="100%"
-        maxW="100%"
-        border=".1px solid #f0f0f0"
-        margin="2rem"
-        borderRadius="10px"
-        overflow="hidden"
-        justifyContent="center"
-        backgroundColor="#fafafa"
-        boxShadow="2px 2px #ddd"
-      >
-        <Image src={pr5} height="200px" width="100%" alt="card image" />
-        <Box>
-          <Badge
-            borderRadius="5px"
-            fontSize=".7rem"
-            padding=".2rem .4rem"
-            backgroundColor="tomato"
-            color="#fff"
-            margin=".5rem 1rem"
-          >
-            New
-          </Badge>
-        </Box>
-        <Box margin="0 1rem">
-          <Text fontSize="1rem" fontWeight="bold">
-            Sneakers
-          </Text>
-          <Text fontSize=".8rem" fontWeight="light" p="1rem 0" opacity="0.8">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
-            possimus nostrum!
-          </Text>
-        </Box>
 
-        <Box margin="0 1rem">
-          <hr margin="0 1rem" />
-          <Text fontSize="1rem" p="1rem 0" color="tomato" fontWeight="bold">
-            $300
-          </Text>
+      {products.map((product, index) => (
+        <Box
+          width="300px"
+          height="100%"
+          maxW="100%"
+          border=".1px solid #f0f0f0"
+          margin="2rem"
+          borderRadius="10px"
+          overflow="hidden"
+          justifyContent="center"
+          backgroundColor="#fafafa"
+          boxShadow="2px 2px #ddd"
+        >
+          <Image
+            src={product.image}
+            height="200px"
+            width="100%"
+            alt="card image"
+          />
+          <Box>
+            <Badge
+              borderRadius="5px"
+              fontSize=".7rem"
+              padding=".2rem .4rem"
+              backgroundColor="tomato"
+              color="#fff"
+              margin=".5rem 1rem"
+            >
+              {product.category}
+            </Badge>
+          </Box>
+          <Box margin="0 1rem">
+            <Text fontSize="1rem" fontWeight="bold">
+              {product.name}
+            </Text>
+            <Text fontSize=".8rem" fontWeight="light" p="1rem 0" opacity="0.8">
+              {product.desc}
+            </Text>
+          </Box>
+
+          <Box margin="0 1rem">
+            <hr margin="0 1rem" />
+            <Text fontSize="1rem" p="1rem 0" color="tomato" fontWeight="bold">
+              ${product.price}
+            </Text>
+          </Box>
         </Box>
-      </Box>
+      ))}
     </Container>
   );
 }
